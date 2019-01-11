5acfcd02313a8df608833efbe660b1cf65ae37f7
PurpleBunny <michael.l33.1994@gmail.com>
2019-01-10 17:21:50 -0800

    Updates

---
+++ precache-manifest.2587d3d91e6089e1bdf91c274d366a75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "17112401062c91f57f91",
+    "url": "/Clicky-Game/static/css/main.c80eb7b2.chunk.css"
+  },
+  {
+    "revision": "17112401062c91f57f91",
+    "url": "/Clicky-Game/static/js/main.17112401.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/Clicky-Game/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Clicky-Game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2eb4d9828d1bae4882359c44d3463698",
+    "url": "/Clicky-Game/index.html"
+  }
+];
